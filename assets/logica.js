0a8699c2b21fe7b432a0dee23718d92da31f1694
Esteban <nameless.voidd@gmail.com>
2025-06-02 15:43:09 -0300

    Actualizacion de datos

--- assets/logica.js
+++ assets/logica.js
@@ -4,16 +4,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //----------------------------------------------------------objeto persona con informacion personal------------------------------------------------------------------------
     let persona={
         nombre:"Esteba Orfeo Candioti",
-        celular:"1135657044",
+        celular:"3425693808",
         email:"Estebancandio@gmail.com",
         ciudad:"Santa Fe Ciudad",
         direccion:"Maipu 1661",
         fotoPerfil:"",
-        instagram:"esteban_candioti",
-        linkedin:"esteban-candioti",
-        sobre_mi:"Estudiante de desarrollo web con interes en aprender y ganar experiencia en el area IT. Enfocado y con gusto por el Front-End",
-        aptitudes:["organizado", "coperador", "activo"],
-        habilidadesInformaticas:["CSS","HTML","JavaScript","React","SpringBoot (basico)","MySql (basico)"],
+        sobre_mi:"Estudiante de desarrollo desoftware con interes en aprender y ganar experiencia en el area IT. Enfocado y con gusto por el Front-End",
+        aptitudes:["Adaptable", "Coperador", "Activo"],
+        habilidadesInformaticas:["CSS","HTML","JavaScript","React","Angular","Java","SpringBoot","Sql"],
         certificados:["Ingles - B1", "Programador Web Full Stack Jr.", "Front End G4"],
     }
 
@@ -21,8 +19,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     var whatsapp = document.getElementById("whatsapp");
     var email= document.getElementById("email");
-    var instagram= document.getElementById("instagram");
-    var linkedin= document.getElementById("linkedin");
     var direccion= document.getElementById("direccion");
     var correo_electronico= document.getElementById("correo_electronico");
     var celular= document.getElementById("celular");
@@ -35,19 +31,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     whatsapp.onclick= function() {ponerWhatsapp()};
     email.onclick= function() {ponerEmail()};
-    instagram.onclick= function(){ponerInstagram()};
-    linkedin.onclick = function(){ponerLinkedin()};
-
-    //---------------------------------------------VARIABLES MOSTRADAS EN CONSOLA PARA ASEGURAR QUE ESTEN SIENDO CAPTURADAS-----------------------------------------------------
-
-    console.log(whatsapp);
-    console.log(email);
-    console.log(instagram);
-    console.log(linkedin);
-    console.log(direccion);
-    console.log(email);
-    console.log(celular);
-    console.log(sobre_mi);
 
     //-----------------------------------------------------------------------------------FUNCIONES--------------------------------------------------------------------------------
 
@@ -60,30 +43,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     function ponerEmail(){
         console.log("mi correo electronico " + persona.email);
         email.href="mailto:" + persona.email;
-    }
-
-    function ponerInstagram(){
-        console.log("mi instagram " + persona.instagram);
-        instagram.href="https://instagram.com/"+persona.instagram;
-        instagram.target="_blank";
-    }
-    
-    function ponerFacebook(){
-        console.log("mi facebook " + persona.facebook);
-        facebook.href="https://facebook.com/"+persona.facebook;
-        facebook.target="_blank";
-    }
-
-    function ponerLinkedin(){
-        console.log("mi linkedin " + persona.linkedin);
-        linkedin.href="https://www.linkedin.com/in/"+persona.linkedin;
-        linkedin.target="_blank";
-    }
-
-    function ponerTwitter(){
-        console.log("mi twitter"+ persona.twitter);
-        twitter.href="https://twitter.com/"+persona.twitter;
-        twitter.target="_blank"
     }
 
     //---------------------------------------------USAR INNER HTML PARA RELLENAR EL PORTFOLIO CON INFORMACION PERSONAL--------------------------------------------------------
